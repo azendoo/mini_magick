9de58e482452529393ad72569cadfe8cf2e035c2
Bastien Murzeau <bmurzeau@azendoo.com>
2012-04-08 10:00:34 +0200

    added proxy support when fetching URI

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -127,7 +127,7 @@ module MiniMagick
         if file_or_url.include?("://")
           require 'open-uri'
           ext ||= File.extname(URI.parse(file_or_url).path)
-          self.read(Kernel::open(file_or_url), ext)
+          self.read(Kernel::open(file_or_url, :proxy => ENV['http_proxy']), ext)
         else
           ext ||= File.extname(file_or_url)
           File.open(file_or_url, "rb") do |f|
